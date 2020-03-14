e76e6ff089b09edce3c5c7603fede89646ccbc0a
inzahan <inzahanyang@gmail.com>
2020-03-14 21:31:49 +0900

    DOM Array Methods

--- DOM-Array-Methods/script.js
+++ DOM-Array-Methods/script.js
@@ -1,9 +1,9 @@
 const main = document.getElementById("main");
 const addUserBtn = document.getElementById("add_user");
 const doubleBtn = document.getElementById("double");
-const showMillionairesBtn = document.getElementById("show-millionaires");
+const showMillionairesBtn = document.getElementById("show_millionaires");
 const sortBtn = document.getElementById("sort");
-const calculateWealthBtn = document.getElementById("calculate-wealth");
+const calculateWealthBtn = document.getElementById("calculate_wealth");
 
 getRandomUser();
 getRandomUser();
@@ -43,6 +43,23 @@ function sortByRichest() {
   updateDOM();
 }
 
+// Filter only showMillionaires
+function showMillionaires() {
+  data = data.filter(user => user.money > 1000000);
+  updateDOM();
+}
+
+// Calculate the total wealth
+function calculateWealth() {
+  const wealth = data.reduce((acc, user) => (acc += user.money), 0);
+
+  const wealthEl = document.createElement("div");
+  wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(wealth)}</strong></h3>`;
+  main.appendChild(wealthEl);
+
+  console.log(wealth);
+}
+
 // Add new obj to data arr
 function addData(obj) {
   data.push(obj);
@@ -71,3 +88,5 @@ function formatMoney(number) {
 addUserBtn.addEventListener("click", getRandomUser);
 doubleBtn.addEventListener("click", doubleMoney);
 sortBtn.addEventListener("click", sortByRichest);
+showMillionairesBtn.addEventListener("click", showMillionaires);
+calculateWealthBtn.addEventListener("click", calculateWealth);
