425c0aea08624760b99fffb7f51d4af8abaee69e
inzahan <inzahanyang@gmail.com>
2020-04-06 22:36:55 +0900

    Hangman

---
+++ HANGMAN/script.js
@@ -0,0 +1,67 @@
+const wordEl = document.getElementById("word");
+const wrongLettersEl = document.getElementById("wrong-letters");
+const playAgainBtn = document.getElementById("play-again");
+const popup = document.getElementById("popup-container");
+const notification = document.getElementById("notification-container");
+const finalMessage = document.getElementById("final-message");
+
+const figureParts = document.querySelectorAll(".figure-part");
+
+const words = ["application", "programming", "interface", "wizard"];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+const correctLetters = ["w", "i", "z", "a", "r", "d"];
+const wrongLetters = [];
+
+function displayWord() {
+  wordEl.innerHTML = `
+        ${selectedWord
+          .split("")
+          .map(letter => `<span class="letter">${correctLetters.includes(letter) ? letter : ""}</span>`)
+          .join("")}
+    `;
+  const innerWord = wordEl.innerText.replace(/\n/g, "");
+
+  if (innerWord === selectedWord) {
+    finalMessage.innerText = "Congratulations! You won! 😝";
+    popup.style.display = "flex";
+  }
+}
+
+// Update the wrong letters
+function updateWrongLettersEl() {
+  console.log("update wrong");
+}
+
+function showNotification() {
+  notification.classList.add("show");
+
+  setTimeout(() => {
+    notification.classList.remove("show");
+  }, 2000);
+}
+
+window.addEventListener("keydown", e => {
+  if (e.keyCode >= 65 && e.keyCode <= 90) {
+    const letter = e.key;
+    if (selectedWord.includes(letter)) {
+      if (!correctLetters.includes(letter)) {
+        correctLetters.push(letter);
+        displayWord();
+      } else {
+        showNotification();
+      }
+    } else {
+      if (!wrongLetters.includes(letter)) {
+        wrongLetters.push(letter);
+
+        updateWrongLettersEl();
+      } else {
+        showNotification();
+      }
+    }
+  }
+});
+
+displayWord();
